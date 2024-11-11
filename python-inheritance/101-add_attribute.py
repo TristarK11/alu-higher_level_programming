bc7820148b9ad85bb3bd31b7e01a3440d12d669e
root <TristarK11>
2024-11-11 06:30:14 -0800

    Program

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,4 +1,5 @@
-def add_attribute(obj, attribute, value):
+#!/usr/bin/python
+3def add_attribute(obj, attribute, value):
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
